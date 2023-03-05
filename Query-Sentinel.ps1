2a44130e7b2f4323bd525cbd6ce5dafa1e00cc07
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-03-04 21:44:14 -0500

    Scrip[t not quite there

--- Query-Sentinel.ps1
+++ Query-Sentinel.ps1
@@ -1,22 +1,54 @@
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$FilePath,
+# [CmdletBinding()]
+# param (
+#     [Parameter(Mandatory=$true)]
+#     [string]$FilePath,
 
-    [Parameter()]
-    [string]$WorkspaceName = "sc200",
+#     [Parameter()]
+#     [string]$WorkspaceName = "sc200",
 
-    [Parameter()]
-    [string]$WorkspaceResourceGroup = "sc-200"
-)
+#     [Parameter()]
+#     [string]$WorkspaceResourceGroup = "sc-200"
+# )
+$WorkspaceName = "sc200"
+$WorkspaceResourceGroup = "sc-200"
 
-Connect-AzAccount
+
+Uninstall-AzureRm
+
+if (!(Get-Module -Name Az.OperationalInsights -ListAvailable)) {
+    Write-Host "Installing Az.OperationalInsights module..."
+    Install-Module -Name Az.OperationalInsights -Scope CurrentUser -Force -AllowClobber
+} else {
+    Write-Host "Az.OperationalInsights module is already installed."
+}
+
+Import-Module Az.OperationalInsights
+
+if (Get-AzContext -ErrorAction SilentlyContinue) {
+    Write-Host "You are already signed in to Azure."
+} else {
+    Write-Host "You are not signed in to Azure. Please sign in."
+    Connect-AzAccount
+}
 
 $sub = Get-AzSubscription
 Set-AzContext -Subscription $sub
-
 $WorkspaceID = (Get-AzOperationalInsightsWorkspace -Name $WorkspaceName -ResourceGroupName $WorkspaceResourceGroup).CustomerId
 
-$query = Get-Content $FilePath
-$kqlQuery = Invoke-AzOperationalInsightsQuery -WorkspaceId $WorkspaceID -Query $query
-$kqlQuery.Results
+
+$FilePath = ".\UEBA\IdentityInfo-FindAccountsPasswordNotRequired.kql"
+
+# Read the contents of the file into an array
+$fileContents = Get-Content $FilePath -Delimiter "`n"
+
+# Join the array elements into a single string, starting from the 5th line
+$query = ($fileContents[4..($fileContents.Length - 1)] -join "`n").Trim()
+
+# Execute the query using Invoke-AzOperationalInsightsQuery
+$result = Invoke-AzOperationalInsightsQuery -WorkspaceId $WorkspaceID -Query $query
+
+$result
+
+
+
+
